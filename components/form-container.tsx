433c9330acb7fc5d496cd665944792018c299f98
Łukasz <lukasz.poczta12@gmail.com>
2023-11-08 18:25:39 +0100

    created clean app

---
+++ components/form-container.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Center, Container, Paper, Title, Text } from '@mantine/core';
+
+const FormContainer = () => {
+  return (
+    <Container size='md' py={20}>
+      <Paper shadow='xl' radius={'lg'}>
+        <Center>
+          <Title order={1}>CetusAcademy</Title>
+        </Center>
+        <Container mt={100}>
+          <Text>Cześć, </Text>
+          <Text>miło że do nas trafiłeś🥰 </Text>
+          <Text>
+            Misją Cetus Academy jest rozwijanie i wspieranie nowych talentów na
+            ich drodze do świata IT.{' '}
+          </Text>
+          <Text>
+            Wypełnij proszę ankietę która pozwoli nam dobrać dla Ciebie
+            odpowiedni program a następnie dołącz do naszego discorda:{' '}
+          </Text>
+        </Container>
+      </Paper>
+    </Container>
+  );
+};
+
+export default FormContainer;
