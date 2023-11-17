22312e2e97d762ebb3b4724dd3b229b06f1325f6
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-11-17 11:18:40 +0100

    added redirect on isSuccess and created path for test page

---
+++ components/UI/icons/calendar-icon.tsx
@@ -0,0 +1,9 @@
+import { IconCalendar } from '@tabler/icons-react';
+import { rem } from '@mantine/core';
+
+export const calendarIcon = (
+	<IconCalendar
+		style={{ width: rem(18), height: rem(18), color: '#44639F' }}
+		stroke={1.5}
+	/>
+);
