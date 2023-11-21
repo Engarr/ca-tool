b13035749041e8d9e46b9ae6b8fe0e00c9f493ee
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-11-21 14:10:56 +0100

    added context to move user beetwen tabels

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import MemberListContextProvider from '@/context/member-list-context';
 
 const KanbanBoard = dynamic(
-  () =>
-    import('../../components/features/admin-panel/kanban-board/kanban-board'),
-  {
-    ssr: false,
-  }
+	() =>
+		import(
+			'../../components/features/admin-panel/kanban-board/kanban-board'
+		),
+	{
+		ssr: false,
+	}
 );
 
 const Page = () => {
-  return (
-    <div>
-      <KanbanBoard />
-    </div>
-  );
+	return (
+		<MemberListContextProvider>
+			<KanbanBoard />
+		</MemberListContextProvider>
+	);
 };
 
 export default Page;
