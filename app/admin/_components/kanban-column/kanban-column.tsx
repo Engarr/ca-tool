8476d2ac77c8aa03f751108a835476d972960c5a
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-11-24 15:23:45 +0100

    change styles for filters inputs

--- app/admin/_components/kanban-column/kanban-column.tsx
+++ app/admin/_components/kanban-column/kanban-column.tsx
@@ -1,5 +1,19 @@
 import React, { useMemo, useState } from 'react';
-import { Card, Divider, Group, ScrollArea, Select, Text } from '@mantine/core';
+import {
+	Card,
+	Divider,
+	Box,
+	Group,
+	ScrollArea,
+	Select,
+	Text,
+	Collapse,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import {
+	IconArrowBadgeDownFilled,
+	IconArrowBadgeUpFilled,
+} from '@tabler/icons-react';
 import classes from './kanban-column.module.css';
 import KanbanMemberCard from '../kanban-member-card/kanban-member-card';
 import { MemberType } from '../../_types/member-type';
@@ -14,23 +28,21 @@ import ProjectBox from '../project-box/project-box';
 import ColumntTitle from '../kanban-column-title/columnt-title';
 
 type KanbanColumnProps = {
-  title: string;
-  column: KanbanColumnType;
-  members: MemberType[];
+	title: string;
+	column: KanbanColumnType;
+	members: MemberType[];
 };
 
 const KanbanColumn = ({ column, title, members }: KanbanColumnProps) => {
 	const [sortValue, setSortValue] = useState('');
 	const [filterValue, setFilterValue] = useState('');
+	const [opened, { toggle }] = useDisclosure(false);
 
 	const filteredAndSortedMembers = useMemo(() => {
 		let newMembersList = members;
-		if (sortValue) {
-			newMembersList = sortUsersByValue(sortValue, members);
-		}
-		if (filterValue) {
+		if (sortValue) newMembersList = sortUsersByValue(sortValue, members);
+		if (filterValue)
 			newMembersList = filterUsersByValue(filterValue, members);
-		}
 		return newMembersList;
 	}, [members, sortValue, filterValue]);
 
@@ -38,76 +50,99 @@ const KanbanColumn = ({ column, title, members }: KanbanColumnProps) => {
 		return filteredAndSortedMembers.map((member) => member.id);
 	}, [filteredAndSortedMembers]);
 
-  const { setNodeRef } = useSortable({
-    id: column.id,
-    data: {
-      type: 'Column',
-      column,
-    },
-  });
+	const { setNodeRef } = useSortable({
+		id: column.id,
+		data: {
+			type: 'Column',
+			column,
+		},
+	});
 
-  return (
-    <Card
-      ref={setNodeRef}
-      key={title}
-      shadow='md'
-      radius='md'
-      withBorder
-      padding='xs'
-      w={title === 'Projekty' ? 500 : 400}>
-      <ColumntTitle memberCount={members.length} title={column.title} />
-			<Group pb={10} pl={10}>
-				<Select
-					onChange={(e) => {
-						if (e) {
-							setSortValue(e);
-						}
-					}}
-					label='Sortuj według rangi'
-					checkIconPosition='right'
-					data={['od najniższej', 'od najwyższej']}
-					disabled={column.title === 'Projekty'}
-				/>
-				<Select
-					onChange={(e) => {
-						if (e) {
-							setFilterValue(e);
-						}
-					}}
-					label='Filtruj specjalizacje'
-					checkIconPosition='right'
-					data={['frontend', 'backend', 'inne', 'wszystkie']}
-					disabled={column.title === 'Projekty'}
-				/>
-			</Group>
-      <Divider py={5} />
-      <ScrollArea scrollbarSize={6} pr={15} mah={650}>
-        {title === 'Projekty' ? (
-          <>
-            {confirmedProjectList.map((project) => (
-              <React.Fragment key={project.id}>
-                <ProjectBox
-                  projectMembers={members.filter(
-                    (member) => member.assignedToProjectId === project.id
-                  )}
-                  projectBox={project}
-                  columnId={column.id}
-                />
-              </React.Fragment>
-            ))}
-          </>
-        ) : (
-          <SortableContext items={membersIds}>
-            {filteredAndSortedMembers.map((member) => (
-              <React.Fragment key={member.id}>
-                <KanbanMemberCard member={member} />
-              </React.Fragment>
-            ))}
-          </SortableContext>
-        )}
-      </ScrollArea>
-    </Card>
-  );
+	return (
+		<Card
+			ref={setNodeRef}
+			key={title}
+			shadow='md'
+			radius='md'
+			withBorder
+			padding='xs'
+			w={title === 'Projekty' ? 500 : 400}
+		>
+			<ColumntTitle memberCount={members.length} title={column.title} />
+			<Box mr={15}>
+				<Group justify='end' mb={10} onClick={toggle}>
+					<Text size='xs' className={classes.arrowIcon}>
+						Pokaż filtry
+					</Text>
+					{opened ? (
+						<IconArrowBadgeUpFilled
+							className={classes.arrowIcon}
+							stroke={1.5}
+							size={15}
+						/>
+					) : (
+						<IconArrowBadgeDownFilled
+							className={classes.arrowIcon}
+							stroke={1.5}
+							size={15}
+						/>
+					)}
+				</Group>
+				<Collapse pb={10} pl={10} in={opened}>
+					<Select
+						onChange={(e) => {
+							if (e) {
+								setSortValue(e);
+							}
+						}}
+						placeholder='Wybierz opcję'
+						label='Sortuj według rangi'
+						checkIconPosition='right'
+						data={['Od najniższej', 'Od najwyższej']}
+					/>
+					<Select
+						onChange={(e) => {
+							if (e) {
+								setFilterValue(e);
+							}
+						}}
+						placeholder='Wybierz opcję'
+						label='Filtruj specjalizacje'
+						checkIconPosition='right'
+						data={['Frontend', 'Backend', 'Inne', 'Wszystkie']}
+					/>
+				</Collapse>
+			</Box>
+			<Divider py={5} />
+			<ScrollArea scrollbarSize={6} pr={15} mah={650}>
+				{title === 'Projekty' ? (
+					<>
+						{confirmedProjectList.map((project) => (
+							<React.Fragment key={project.id}>
+								<ProjectBox
+									projectMembers={filteredAndSortedMembers.filter(
+										(member) =>
+											member.assignedToProjectId ===
+											project.id
+									)}
+									projectBox={project}
+									columnId={column.id}
+								/>
+							</React.Fragment>
+						))}
+					</>
+				) : (
+					<SortableContext items={membersIds}>
+						{filteredAndSortedMembers.map((member) => (
+							<React.Fragment key={member.id}>
+								<KanbanMemberCard member={member} />
+							</React.Fragment>
+						))}
+					</SortableContext>
+				)}
+			</ScrollArea>
+		</Card>
+	);
 };
 
 export default KanbanColumn;
--- app/admin/_lib/members-list-management-functions.ts
+++ app/admin/_lib/members-list-management-functions.ts
@@ -2,10 +2,10 @@ import { MemberType } from '../_types/member-type';
 
 export const sortUsersByValue = (sortValue: string, members: MemberType[]) => {
 	let sortedUsers;
-	if (sortValue === 'od najwyższej') {
+	if (sortValue === 'Od najwyższej') {
 		sortedUsers = members.sort((a, b) => b.range - a.range);
 		return sortedUsers;
-	} else if (sortValue === 'od najniższej') {
+	} else if (sortValue === 'Od najniższej') {
 		sortedUsers = members.sort((a, b) => a.range - b.range);
 		return sortedUsers;
 	} else return members;
@@ -15,19 +15,19 @@ export const filterUsersByValue = (
 	members: MemberType[]
 ) => {
 	let filteredUsers;
-	if (filterValue === 'frontend') {
+	if (filterValue === 'Frontend') {
 		filteredUsers = members.filter(
 			(user) =>
 				user.specialization && user.specialization.domain === 'Frontend'
 		);
 		return filteredUsers;
-	} else if (filterValue === 'backend') {
+	} else if (filterValue === 'Backend') {
 		filteredUsers = members.filter(
 			(user) =>
 				user.specialization && user.specialization.domain === 'Backend'
 		);
 		return filteredUsers;
-	} else if (filterValue === 'inne') {
+	} else if (filterValue === 'Inne') {
 		filteredUsers = members.filter(
 			(user) =>
 				user.specialization && user.specialization.domain === 'Others'
